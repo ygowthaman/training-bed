66bb8ff8996184f235da72b80e729fde1e7d42d7
Y Gowthaman <ygowthaman@outlook.com>
2022-09-30 15:25:00 -0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeLandingComponent } from './components/recipe/recipe-landing.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeLandingComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/recipe/recipe-landing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-recipe-landing',
+    templateUrl: './recipe-landing.component.html',
+    styleUrls: ['./recipe-landing.component.scss']
+})
+export class RecipeLandingComponent {
+
+}
