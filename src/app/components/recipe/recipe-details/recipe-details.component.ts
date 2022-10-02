ac6df3c711374ee2410dd35c0ecc28bef41eddf7
ygowthaman <yazh_isai@hotmail.com>
2022-10-02 16:04:58 -0700

    Recipe details page

--- src/app/components/recipe/recipe-details/recipe-details.component.ts
+++ src/app/components/recipe/recipe-details/recipe-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../../../models/recipe.model';
-import { FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Ingredient } from 'src/app/models/ingredient';
 
 @Component({
   selector: 'app-recipe-details',
@@ -10,15 +11,24 @@ import { FormControl } from '@angular/forms';
 export class RecipeDetailsComponent implements OnInit {
 
   @Input() recipe: Recipe = new Recipe('', '');
-  ingredientName = new FormControl('');
-  ingredientQuantity = new FormControl('0');
 
-  constructor() { }
+  recepFormGroup: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.recepFormGroup = formBuilder.group({
+      ingredientName: ['', Validators.required],
+      ingredientQuantity: ['0', Validators.min(1)]
+    });
+  }
 
   ngOnInit(): void {
   }
 
   addIngredient() {
-
+    this.recipe.ingredients.push(new Ingredient(this.recepFormGroup.value.ingredientName, this.recepFormGroup.value.ingredientQuantity))
+    this.recepFormGroup.setValue({
+      ingredientName: '',
+      ingredientQuantity: 0
+    });
   }
 }
